54be3a4a24fa89494d9a961527fcd86ddd2ec00e
Luis Alejandro Bermudez <142993857+Alejobermudezb@users.noreply.github.com>
2023-10-13 19:40:09 +0200

    Revision app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 /* eslint-disable */
+
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-//write your code here
+//Generador de Excusas
+
 let generateExcuse = () => {
   let pronoun = ["A", "The"]; // Position: [0, 1] Largo: 2
-  let subject = ["jogger", "racoon", "dog", "driver", "comedian", "pincone"]; // Lentgh: 6
-  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"]; // Lentgh: 5
-  let possetion = ["homework", "toe", "car", "shoe"]; // Lentgh: 4
-  let where = ["on the street", "in my house", "in my driveway"]; // Lentgh: 3
+  let subject = ["jogger", 
+                 "racoon", 
+                 "dog", 
+                 "driver", 
+                 "comedian", 
+                 "pincone"]; // Lentgh: 6
+  let action = ["took my", 
+                "threw my", 
+                "yelled at my", 
+                "stole my", 
+                "bit my"]; // Lentgh: 5
+  let possetion = ["homework", 
+                   "toe", 
+                   "car", 
+                   "shoe"]; // Lentgh: 4
+  let where = ["on the street", 
+               "in my house", 
+               "in my driveway"]; // Lentgh: 3
 
   let pronounIndex = Math.floor(Math.random() * pronoun.length); //0 1 < 2
   let subjectIndex = Math.floor(Math.random() * subject.length);
@@ -20,13 +36,21 @@ let generateExcuse = () => {
   let whereIndex = Math.floor(Math.random() * where.length);
   return (
     pronoun[pronounIndex] +
+    
     " " +
+    
     subject[subjectIndex] +
+    
     " " +
+    
     action[actionIndex] +
+    
     " " +
+    
     possetion[possetionIndex] +
+    
     " " +
+    
     where[whereIndex]
   );
 };
